09ff30f77250557b6d5827c4c2c582511588b095
Artyom Belikin <biloplus@yandex.ru>
2025-06-04 16:38:27 +0300

    First

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,15 +1,43 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
 
     @BeforeAll
     public static void beforeAll() {
-        Configuration.browserSize = System.getProperty("browser.size", "2560x1440");
+
+        String selenoidHost = System.getProperty("selenoid_host", "selenoid.autotests.cloud");
+        String selenoidLogin = System.getProperty("selenoid_login", "user1");
+        String selenoidPassword = System.getProperty("selenoid_password", "1234");
+        String browserVersion = System.getProperty("browserVersion", "127.0");
+        String BROWSER = System.getProperty("browser", "chrome");
+        String BROWSER_SIZE = System.getProperty("browser.size", "2560x1440");
+
+        Configuration.browser = BROWSER;
+        Configuration.browserVersion = browserVersion;
+        Configuration.browserSize = BROWSER_SIZE;
         Configuration.baseUrl = "https://dreamkas.ru";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.headless = false;
+        Configuration.headless = true;
+        Configuration.holdBrowserOpen = false;
+        Configuration.remote = String.format("https://%s:%s@%s/wd/hub",
+                selenoidLogin,
+                selenoidPassword,
+                selenoidHost);
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
     }
 }
